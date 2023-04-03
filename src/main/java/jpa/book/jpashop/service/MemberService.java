f0da222684c96b3a31935bab6d81f00fc167995a
jueon <wndjs803@naver.com>
2023-04-04 01:02:35 +0900

    MemberService

---
+++ src/main/java/jpa/book/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpa.book.jpashop.service;
+
+import jpa.book.jpashop.domain.Member;
+import jpa.book.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member){
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memeberId){
+        return memberRepository.findOne(memeberId);
+    }
+}
