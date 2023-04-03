d411a8f71ccf9ce4c35a13b2b77594b35c9a881d
jueon <wndjs803@naver.com>
2023-04-04 00:13:04 +0900

    Initial commit

---
+++ src/main/java/jpa/book/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package jpa.book.jpashop.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/jpa/book/jpashop/hello.java
@@ -0,0 +1,9 @@
+package jpa.book.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class hello {
+    private String data;
+}
