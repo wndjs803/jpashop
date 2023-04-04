5f0180f088327af2b40d4fa07e94bfeeffdb5ff4
jueon <wndjs803@naver.com>
2023-04-04 15:32:56 +0900

    Item_BusinessLogic

--- src/main/java/jpa/book/jpashop/domain/Item/Item.java
+++ src/main/java/jpa/book/jpashop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package jpa.book.jpashop.domain.Item;
 
 import jpa.book.jpashop.domain.Category;
+import jpa.book.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//
+    // 재고 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
