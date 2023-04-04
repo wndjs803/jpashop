98410e51b62e24c38bf71715a03736a8bb82be3b
jueon <wndjs803@naver.com>
2023-04-04 16:25:04 +0900

    OrderRepository

---
+++ src/main/java/jpa/book/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package jpa.book.jpashop.repository;
+
+import jpa.book.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch){}
+}
